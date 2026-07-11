e78a56b4f0fa3ca0213caa9528bdb41afe034723
Dementij <dementij.email@gmail.com>
2026-07-11 18:52:33 +0300

    Added a License
    Realised that I'm not following the license of GRUB (Technically I'm not redistributing any of it's source or binary files, but it's better to be safe that sorry) so I added a GPLv3.0-only license

--- kernel/src/boot.asm
+++ kernel/src/boot.asm
@@ -1,3 +1,20 @@
+/*
+  bip - Bip Is Perfect
+  Copyright (C) 2026  obrigani team
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation version 3 of the License
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 // Constants for the Multiboot header
 .set ALIGN,    1<<0             // Align loaded modules on page boundaries
 .set MEMINFO,  1<<1             // Provide the memory map
